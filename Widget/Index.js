3ddd2b77204d1cf63e5673026b3074bf2200c3c6
taylordeniseperry <taylordeniseperry@gmail.com>
2016-07-21 23:48:47 -0400

    Widget

---
+++ Widget/Index.js
@@ -0,0 +1,30 @@
+$(document).ready(function($) {
+
+  // Upload btn
+  $(".uploadlogo").change(function() {
+    readURL(this);
+  });
+
+  function readURL(input) {
+    var url = input.value;
+    var ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
+    if (input.files && input.files[0] && (ext == "png" || ext == "jpeg" || ext == "jpg" || ext == "gif" || ext == "svg")) {
+      var path = $('.uploadlogo').val();
+      var filename = path.replace(/^.*\\/, "");
+      $('.fileUpload span').html(filename);
+      // console.log(filename);
+    } else {
+      $(".uploadlogo").val("");
+      $('.fileUpload span').html('Only Images Are Allowed!');
+    }
+  }
+
+  $('.btn').on('click', function() {
+    var $this = $(this);
+  $this.button('loading');
+    setTimeout(function() {
+       $this.button('reset');
+   }, 8000);
+});
+
+});
